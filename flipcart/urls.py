9ffde41891523ee0129a2e8ed10ea799e22d47b7
mariaali <mariaasghar12222@gmail.com>
2023-07-13 20:27:45 +0000

    added update form

--- flipcart/urls.py
+++ flipcart/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import path
 from flipcart import views
-from . views import signin,hello_world,signout,home,signup,msg
+from . views import signin,hello_world,signout,home,signup,msg,page1,dlt,update
 
 urlpatterns = [
     # Other URL patterns
@@ -17,6 +17,11 @@ urlpatterns = [
     path('hello/', hello_world, name='hello_world'),
     path('signout/', views.signout, name='signout'),
     path('msg/', views.msg, name='msg'),
+    path('page1/', views.page1, name='page1'),
+    path("<int:user_id>/dlt/", views.dlt, name="dlt"),
+    path("<int:user_id>/update/", views.update, name="update"),
     
 ]
+
+
 
--- flipcart/views.py
+++ flipcart/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.admin.views.decorators import staff_member_required
+
 def home(request):
     return render(request, "flipcart/home.html")
 
@@ -33,39 +35,41 @@ def signup(request):
             return redirect('msg')
 
     return render(request, "flipcart/signup.html")
+
 def signin(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
 
         user = authenticate(username=username, password=password)
-
-        if user is not None:
+        if user is not None and user.is_superuser:
             login(request, user)
-            return redirect('hello_world')
+            users = User.objects.all()
+            context = {
+                'users': users,
+                'can_delete_update': True,  # Set a flag to indicate the superuser can delete and update
+            }
+            return render(request, "flipcart/db.html", context)
+        elif user is not None and user.is_authenticated:
+            login(request, user)
+            return render(request, "flipcart/db.html")
         else:
-            error_message = "Invalid username or password."  # Add an error message if authentication fails
+            error_message = "Invalid username or password."
             return render(request, "flipcart/invalid.html", {'error_message': error_message})
 
     return render(request, "flipcart/signin.html")
 
+
+
 @login_required(login_url="/signin")
+def page1(request):   
+    return render(request, "flipcart/hello.html")
 def hello_world(request):
-    current_user = request.user
-    user_id = current_user.id
-    user_username = current_user.username
-    user_email = current_user.email
     users = User.objects.all()
 
     context = {
-        'clist':[user_id,user_username,user_email],
-        'student_detail':{
-        'user_id': user_id,
-        'user_username': user_username,
-        'user_email': user_email,
-            
-        }
- 
+        'users': users
+
     }
 
 
@@ -80,3 +84,81 @@ def signout(request):
     return render(request, "flipcart/signout.html")
 def msg(request):
     return render(request, "flipcart/msg.html")
+"""
+def admin_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user is not None and user.is_superuser:
+            login(request, user)
+            return redirect('hello_world')
+        else:
+            error_message = "Invalid username or password."
+            return HttpResponse("invalid user")
+
+    return render(request, 'flipcart/login.html')
+
+"""
+""""
+@login_required(login_url="/signin")
+def create_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        dbirth = request.POST['date_of_birth']
+        email = request.POST['email']
+        password = request.POST['password']
+        cpassword = request.POST['cpassword']
+
+        if password != cpassword:
+            return HttpResponse("Your password and confirm password are not the same!")
+        else:
+            # Check if the username or email already exists
+            if User.objects.filter(username=username).exists():
+                return HttpResponse("Username already exists. Please choose a different username.")
+            if User.objects.filter(email=email).exists():
+                return HttpResponse("Email is already registered. Please use a different email.")
+
+            # Create a new user
+            myuser = User.objects.create_user(username, email, password,)
+            # Save the user
+            myuser.save()
+
+            # Redirect to the home page or any other desired page
+            return redirect('msg')
+"""
+@login_required
+def dlt(request,user_id): 
+    try:
+        
+        user = User.objects.get(id=user_id)
+        user.delete()
+        messages.success(request, "The user is deleted.")
+        return render(request, "flipcart/db.html")
+    except User.DoesNotExist:
+        messages.error(request, "The user does not exist.")
+    return render(request, "flipcart/db.html")
+
+
+@login_required
+@login_required
+def update(request, user_id):
+    
+    try:
+        user = User.objects.get(id=user_id)
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        date_of_birth = request.POST.get('date_of_birth')
+
+            # Check if the username is provided
+        if username:
+            user.username = username
+            user.email = email
+            user.date_of_birth = date_of_birth
+            user.save()
+
+            messages.success(request, "User updated successfully.")   
+    except User.DoesNotExist:
+        messages.error(request, "The user does not exist.")
+    return render(request, "flipcart/db.html")
