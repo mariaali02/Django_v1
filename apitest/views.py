b6dbc6cef72b02669d264fc92b481609baa88e40
mariaali <mariaasghar12222@gmail.com>
2023-08-01 20:06:29 +0000

    put function of api created

--- apitest/views.py
+++ apitest/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from rest_framework.response import Response
 from .serializers import UserSerializer
+from rest_framework import status
 
 class UserListView(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -29,6 +30,28 @@ class UserDetail1(APIView):
         serializer = UserSerializer(queryset, many=True)
         
         return Response(serializer.data)
+
+    def put(self, request, format=None):        
+        userId = int(request.query_params['userId']) if request.query_params['userId'] else None
+        dicUser = {}
+        dicUser["email"] = request.data['email']
+        #dicUser["username"] = request.data['username']
+        try:
+            if userId:
+                objUser = User.objects.filter(pk=userId)
+                res = objUser.update(**dicUser)
+                if res:
+                # serializer = UserSerializer(objUser, data=request.data)
+                # if serializer.is_valid():
+                #     serializer.save(5)
+                    return Response({'message': 'saved successfully.'}, status=status.HTTP_205_RESET_CONTENT)
+            else:
+                return Response({'message': 'error.'}, status=status.HTTP_400_BAD_REQUEST)
+
+            #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as ex:
+            return Response({'message': 'error.'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 class Hello(APIView):
     permission_classes = [permissions.AllowAny]
