0bfd11cd04246ff778b8b4353150b907fc55db07
mariaali <mariaasghar12222@gmail.com>
2023-09-11 20:20:31 +0000

    added function to get username to the chat room function

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,5 +1,9 @@
 import json
+
+from django.contrib.auth.models import User
 from channels.generic.websocket import AsyncWebsocketConsumer
+from asgiref.sync import sync_to_async
+
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -11,36 +15,43 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
-        print("socket is conneted")
+
         await self.accept()
 
-    async def disconnect(self, close_code):
+    async def disconnect(self):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
-        print("socket is disconneted")
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        #username = text_data_json['username']
 
+    # Receive message from WebSocket
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        print(data)
+        message = data['message']
+        username = data['username']
+        room = data['username']
+
+        await self.save_message(username, room, message)
+
+        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'chatroom_message',
+                'type': 'chat_message',
                 'message': message,
-                #'username': username,
+                'username': username
             }
         )
-        print("msg is received")
-    async def chatroom_message(self, event):
-        message = event['message']
-        #username = event['username']
 
+    # Receive message from room group
+    async def chat_message(self, event):
+        message = event['message']
+        username = event['username']
+
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
-            #'username': username,
+            'username': username
         }))
 
-    pass
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from flipcart.models import UserProfile
+from apitest.serializers import UserSerializer,UserProfileSerializer
 
 def indext(request):
     return render(request, 'flipcart/indext.html', {})
 
 
+
 def room(request, room_name):
+    # Initialize an empty data dictionary
+    data = {}
+
+    # Check if the user is authenticated
+    if request.user.is_authenticated:
+        userId = request.GET.get('userId')
+
+        # Check if userId is provided in the request
+        if userId:
+            try:
+                # Attempt to retrieve the user by primary key (userId)
+                objUser = User.objects.get(User, pk=userId)
+                serializer_user = UserSerializer(objUser)
+                data['user'] = serializer_user.data['username']
+            except User.DoesNotExist:
+                # Handle the case where the user does not exist
+                data['user'] = 'User not found'
+
     return render(request, 'flipcart/chatroom.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'data': data,  # Pass the data to the template
     })
