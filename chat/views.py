277d072ae90d4a83fde422c90111c09409c3b89d
mariaali <mariaasghar12222@gmail.com>
2023-09-13 20:14:50 +0000

    provided user id to chat button and fix the issue of redis

--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from flipcart.models import UserProfile
 from apitest.serializers import UserSerializer,UserProfileSerializer
+from django.http import HttpResponseNotFound
 
 def indext(request):
     return render(request, 'flipcart/indext.html', {})
@@ -9,25 +10,25 @@ def indext(request):
 
 
 def room(request, room_name):
-    # Initialize an empty data dictionary
     data = {}
-
-    # Check if the user is authenticated
     if request.user.is_authenticated:
         userId = request.GET.get('userId')
-
-        # Check if userId is provided in the request
         if userId:
             try:
-                # Attempt to retrieve the user by primary key (userId)
-                objUser = User.objects.get(User, pk=userId)
+                objUser = User.objects.get(pk=userId)
                 serializer_user = UserSerializer(objUser)
                 data['user'] = serializer_user.data['username']
             except User.DoesNotExist:
                 # Handle the case where the user does not exist
                 data['user'] = 'User not found'
+        else:
+            # Handle the case where 'userId' is not provided in the request
+            data['user'] = 'User ID not provided'
 
-    return render(request, 'flipcart/chatroom.html', {
-        'room_name': room_name,
-        'data': data,  # Pass the data to the template
-    })
+        return render(request, 'flipcart/chatroom.html', {
+            'room_name': room_name,
+            'data': data,  # Pass the data to the template
+        })
+    else:
+        # Handle the case where the user is not authenticated
+        return HttpResponseNotFound('Authentication required')
