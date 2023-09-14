c083c7392e555894dc03217b3748bfe24243c6d6
mariaali <mariaasghar12222@gmail.com>
2023-09-14 20:17:12 +0000

    messages sended to the given username

--- chat/consumers.py
+++ chat/consumers.py
@@ -18,32 +18,48 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-    async def disconnect(self):
+    async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
+
     # Receive message from WebSocket
     async def receive(self, text_data):
         data = json.loads(text_data)
+        username = data.get('username')  # Separate variable for username
+        message = data.get('message')  # Use .get() to safely access the message field
+        sender = data.get('sender')  # Use .get() to safely access the message field
+        
+        # room = data.get('room/room_name')  # Separate variable for the room name
+
+        print("data")
         print(data)
-        message = data['message']
-        username = data['username']
-        room = data['username']
-
-        await self.save_message(username, room, message)
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'username': username
-            }
-        )
-
+        
+        if username and message:
+            # Send message to room group
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    
+                    'type': 'chat_message',
+                    'message': message,
+                    'username': username,
+                    'sender' : sender,
+                }
+            )
+        else:
+            # Handle missing or incomplete data
+            print("Invalid or incomplete data received.")
+    
+    async def sendMessage(self , event) :
+        message = event["message"]
+        username = event["username"]
+        sender = event["sender"]
+        
+        await self.send(text_data = json.dumps({"sender" : sender , "message":message ,"username":username}))
+        print(0)
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
--- chat/views.py
+++ chat/views.py
@@ -8,27 +8,31 @@ def indext(request):
     return render(request, 'flipcart/indext.html', {})
 
 
-
 def room(request, room_name):
-    data = {}
-    if request.user.is_authenticated:
-        userId = request.GET.get('userId')
-        if userId:
-            try:
-                objUser = User.objects.get(pk=userId)
-                serializer_user = UserSerializer(objUser)
-                data['user'] = serializer_user.data['username']
-            except User.DoesNotExist:
-                # Handle the case where the user does not exist
-                data['user'] = 'User not found'
-        else:
-            # Handle the case where 'userId' is not provided in the request
-            data['user'] = 'User ID not provided'
+    return render(request, 'flipcart/chatroom.html', {
+        'room_name': room_name
+    })
 
-        return render(request, 'flipcart/chatroom.html', {
-            'room_name': room_name,
-            'data': data,  # Pass the data to the template
-        })
-    else:
-        # Handle the case where the user is not authenticated
-        return HttpResponseNotFound('Authentication required')
+# def room(request, room_name):
+#     data = {}
+#     if request.user.is_authenticated:
+#         userId = request.GET.get('userId')
+#         if userId:
+#             try:
+#                 objUser = User.objects.get(pk=userId)
+#                 serializer_user = UserSerializer(objUser)
+#                 data['user'] = serializer_user.data['username']
+#             except User.DoesNotExist:
+#                 # Handle the case where the user does not exist
+#                 data['user'] = 'User not found'
+#         else:
+#             # Handle the case where 'userId' is not provided in the request
+#             data['user'] = 'User ID not provided'
+
+#         return render(request, 'flipcart/chatroom.html', {
+#             'room_name': room_name,
+#             'data': data,  # Pass the data to the template
+#         })
+#     else:
+#         # Handle the case where the user is not authenticated
+#         return HttpResponseNotFound('Authentication required')
